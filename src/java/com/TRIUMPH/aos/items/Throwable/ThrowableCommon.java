fb931c37667da6e32b0eaa620b86069e5bba3f68
Fariko-Dungeonz <theextr3megamer@gmail.com>
2015-12-28 20:29:26 +1000

    [ReSync]

--- src/java/com/TRIUMPH/aos/items/Throwable/ThrowableCommon.java
+++
@@ -1,9 +0,0 @@
-package com.TRIUMPH.aos.items.Throwable;
-
-/**
- * Created by Bluewolf on 24/12/2015.
- */
-public class ThrowableCommon {
-    public void registerRenderThings(){}
-    public void registerSounds(){}
-}
