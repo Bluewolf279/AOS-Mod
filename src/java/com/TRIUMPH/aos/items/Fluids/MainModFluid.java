392a6dca74d46deb273bdcd0119f217edb690a1c
Bluewolf <Tracymatt2@hotmail.co.uk>
2015-12-28 13:01:35 +1100

    Added Nitroglycerin texture flow Fixed various bugs

--- src/java/com/TRIUMPH/aos/items/Fluids/MainModFluid.java
+++ src/java/com/TRIUMPH/aos/items/Fluids/MainModFluid.java
@@ -1,7 +1,6 @@
 package com.TRIUMPH.aos.items.Fluids;
 
 import com.TRIUMPH.aos.unLocalizedNames;
-import net.minecraft.util.ResourceLocation;
 import net.minecraftforge.fluids.Fluid;
 import net.minecraftforge.fluids.FluidRegistry;
 
@@ -12,7 +11,7 @@ import net.minecraftforge.fluids.FluidRegistry;
 public class MainModFluid {
     public static Fluid Nitroglycerin;
     public static void createFluids(){
-        FluidRegistry.registerFluid(Nitroglycerin=new ModFluid(unLocalizedNames.FluidNitroglycerin, new ResourceLocation("aos:blocks/fluid/Nitroglycerin_still"),new ResourceLocation("blocks/water_flow"),0xffffafaf,295,3000));
+        FluidRegistry.registerFluid(Nitroglycerin=new ModFluid(unLocalizedNames.FluidNitroglycerin,0xffffafaf,295,3000));
     }
     public int hexToInt(String hex){
         int value = Integer.parseInt(hex, 16);
